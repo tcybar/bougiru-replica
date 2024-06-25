716d9d5cd346f2625ce3011722483e16303ae88c
tcybar <tcybars@gmail.com>
2024-06-25 09:38:45 +0900

    :wrench: eslintの環境構築

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 const TextInput = () => {
-    const [text, setText] = useState("");
-    return (
-        <div>
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)} aria-label="text input" />
-            <p>entered text: {text}</p>
-        </div>
-
-    );
-}
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        aria-label="text input"
+      />
+      <p>entered text: {text}</p>
+    </div>
+  );
+};
 
 export default TextInput;
